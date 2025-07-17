8a307e44883107e7eb0e52abb1d0917263c05e93
Saulo Brustolin <saulo.brustolin@ampla.ind.br>
2025-07-17 17:23:26 -0300

    start

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { chromium } from 'playwright';
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto('https://example.com');
+
+  const title = await page.title();
+  console.log('Page title:', title);
+
+  await browser.close();
+})();
